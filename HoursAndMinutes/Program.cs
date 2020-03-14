27a9477b06d4d795c6694184b342ebca643ba745
magnetoplasmadynamics <link1922@gmail.com>
2020-03-14 15:48:28 -0500

    message

---
+++ HoursAndMinutes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoursAndMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutes;
+            Console.WriteLine("Please enter a number of minutes to convert to Hours and Minutes: ");
+            minutes = Convert.ToInt32(Console.ReadLine());
+            int hours = minutes / 60;
+            minutes = minutes % 60;
+            Console.WriteLine("hours: " + hours + " and minutes: " + minutes);
+            Console.ReadLine();
+
+
+        }
+    }
+}
